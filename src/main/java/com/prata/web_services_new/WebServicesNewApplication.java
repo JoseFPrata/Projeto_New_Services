5f771afc334be87d68aed4c5c74480308e5d6b5e
JoseFPrata <ferre22009@hotmail.com>
2022-07-01 16:52:08 -0300

    Projeto Web Services New criado

---
+++ src/main/java/com/prata/web_services_new/WebServicesNewApplication.java
@@ -0,0 +1,13 @@
+package com.prata.web_services_new;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServicesNewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServicesNewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prata/web_services_new/WebServicesNewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prata.web_services_new;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServicesNewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
