5a47e38dd3d555563c1f76292d207112c13b8cb1
JoseFPrata <ferre22009@hotmail.com>
2022-07-02 10:03:31 -0300

    Criando tabela no H2. Erro corrigido user como nome de tabela não é válido no SQL

--- src/main/java/com/prata/web_services_new/resources/UserResource.java
+++ src/main/java/com/prata/web_services_new/resources/UserResource.java
@@ -5,15 +5,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.prata.web_services_new.entities.User;
+import com.prata.web_services_new.entities.Usuario;
 
 @RestController
 @RequestMapping(value="/users")
 public class UserResource {
     
 	@GetMapping
-	public ResponseEntity<User> FindAll() {
-		User u = new User(1L,"Maria","maria@gmail.com","99999","12345");
+	public ResponseEntity<Usuario> FindAll() {
+		Usuario u = new Usuario(1L,"Maria","maria@gmail.com","99999","12345");
 		return ResponseEntity.ok().body(u);
 	}
 
